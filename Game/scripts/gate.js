efa2e65d8fc4a1e3e15a047629d346016623ad01
Chris <c.i.cristakos@gmail.com>
2022-12-18 16:36:34 +0200

    Fixed AND gate

--- Game/scripts/gate.js
+++ Game/scripts/gate.js
@@ -1,29 +1,34 @@
-class Gate{
-    constructor(type){
+class Gate {
+    constructor(type) {
         this.type = type || AND;
         this.inputs = {}
         this.node = new Node();
         this.state = this.node.state;
     }
-    setInput(lbl, element){
+    setInput(lbl, element) {
         this.inputs[lbl] = element.node;
     }
-    update(){
+    update() {
         let vals = Object.values(this.inputs).map(node => node.state);
-        if(this.type == AND){
-            this.state = false;
-            for(let i=0; i<vals.length; i++){
-                this.state = this.state & vals[i];
+        if (this.type == AND) {
+            if (Object.keys(this.inputs).length > 1) {
+                this.state = this.inputs[0].state;
+                for (let i = 1; i < vals.length; i++) {
+                    this.state = this.state & vals[i];
+                }
+            }else{
+                this.state = this.inputs[0].state;
             }
+
         }
-        else if(this.type == OR){
+        else if (this.type == OR) {
             this.state = false;
-            for(let i=0; i<vals.length; i++){
+            for (let i = 0; i < vals.length; i++) {
                 this.state = this.state | vals[i];
             }
         }
-        else if(this.type == NOT){
-            if(vals.length == 1){
+        else if (this.type == NOT) {
+            if (vals.length == 1) {
                 this.state = !vals[0];
             }
         }
